db1a09aecf728cd8e4406628686cea7020196081
MANIKANDAN-P-WEB <manikandanp5657@gmail.com>
2025-12-18 10:08:09 +0530

    commit 1

---
+++ tnsprojects/src/com/tnsif/oops/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+
+
+package com.tnsif.oops.encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        OopsConceptDemo obj= new OopsConceptDemo();
+        obj.setSerialNum(1001);
+        obj.setName("Manikandan");
+        obj.setAge(21);
+        System.out.println(obj);
+	}
+
+}
--- tnsprojects/src/tnsprojects/While.java
+++ tnsprojects/src/tnsprojects/While.java
@@ -3,7 +3,7 @@ package tnsprojects;
 public class While {
 		public static void main(String[] args) 
 		{
-			int i=79;
+			int i=99;
 			while(i<=100)
 			{
 				System.out.println(i);
